47adebd66b81dc547d9b61c7b3312f982c03e848
Darryl Whitmore <darryl.whitmore@gmail.com>
2024-11-06 18:38:08 -0600

    implement option to output json to a file

--- Program.cs
+++ Program.cs
@@ -19,34 +19,52 @@ namespace Mp3TagReader {
 				"The MP3 file(s) specification. Wildcards may be used.",
 				CommandOptionType.SingleValue );
 
+			var outputFolderOption = app.Option( "-of|--outputFolder <Folder>",
+				"Optional destination folder to write output JSON file(s). If omitted, the MP3 source folder will be used.",
+				CommandOptionType.SingleOrNoValue );
+
 			app.OnExecute( () => {
 				if ( fileSpecOption.HasValue() ) {
 					var fileSpecValue = fileSpecOption.Value() ?? string.Empty;
 
-					var directory = Path.GetDirectoryName( fileSpecValue );
+					var mp3Directory = Path.GetDirectoryName( fileSpecValue );
 
-					if ( !Directory.Exists( directory ) ) {
-						Console.WriteLine( $"Directory does not exist: '{directory}'" );
+					if ( !Directory.Exists( mp3Directory ) ) {
+						Console.WriteLine( $"MP3 directory does not exist: '{mp3Directory}'" );
 						return AppReturnValueFail;
 					}
 
 					var searchPattern = Path.GetFileName( fileSpecValue );
 
-					var files = Directory.GetFiles( directory, searchPattern );
+					var mp3Files = Directory.GetFiles( mp3Directory, searchPattern );
 
-					if ( !files.Any() ) {
-						Console.WriteLine( "No files were found." );
+					if ( !mp3Files.Any() ) {
+						Console.WriteLine( "No MP3 files were found." );
 						return AppReturnValueFail;
 					}
 
-					foreach ( var file in files ) {
+					foreach ( var file in mp3Files ) {
 						if ( Path.GetExtension( file ) != ".mp3" ) {
 							Console.WriteLine( $"A non-MP3 file was found: '{file}'" );
 							return AppReturnValueFail;
 						}
 					}
 
-					return Process( files );
+					var outputFolder = string.Empty;
+					if ( outputFolderOption.HasValue() ) {
+						outputFolder = outputFolderOption.Value();
+
+						if ( string.IsNullOrWhiteSpace( outputFolder ) ) {
+							outputFolder = mp3Directory;
+						}
+
+						if ( !Directory.Exists( outputFolder ) ) {
+							Console.WriteLine( $"Output folder does not exist: '{outputFolder}'" );
+							return AppReturnValueFail;
+						}
+					}
+
+					return Process( mp3Files, outputFolder );
 				}
 
 				Console.WriteLine( "One or more required arguments were not provided." );
@@ -64,12 +82,29 @@ namespace Mp3TagReader {
 			return AppReturnValueFail;
 		}
 
-		private static int Process( string[] files ) {
-			var tags = ( from file in files select new Id3Tag( file ) ).ToList();
+		private static int Process( string[] mp3Files, string outputFolder ) {
+			foreach ( var mp3File in mp3Files ) {
+				try {
+					var tag = new Id3Tag( mp3File );
 
-			var json = JsonConvert.SerializeObject( tags, Formatting.Indented );
+					var json = JsonConvert.SerializeObject( tag, Formatting.Indented );
 
-			Console.WriteLine( json );
+					if ( string.IsNullOrEmpty( outputFolder ) ) {
+						Console.WriteLine( json );
+					}
+					else {
+						var jsonFile = Path.Combine( outputFolder, Path.GetFileNameWithoutExtension( mp3File ) + ".json" );
+
+						Console.WriteLine( $"Writing tag info to '{jsonFile}'." );
+
+						File.WriteAllText( jsonFile, json );
+					}
+				}
+				catch ( Exception e ) {
+					Console.WriteLine( e.Message );
+					return AppReturnValueFail;
+				}
+			}
 
 			return AppReturnValueOk;
 		}
