9919d88d8a7303efe1024fc4dc70a1446814bbce
Darryl Whitmore <darryl.whitmore@gmail.com>
2024-12-05 01:47:12 -0600

    Feature: Add support for ID3v1 tag reading - Introduced `Id3V1Tag` class to read ID3v1 tags from MP3 files. - Updated `Mp3TagInfo` to load both ID3v1 and ID3v2 tags. - Modified `launchSettings.json` to include new profiles for testing.

---
+++ Id3V1Tag.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+
+namespace Mp3TagReader {
+	// What are ID3 Tags all about?
+	// ID3v1 tag
+	// https://phoxis.org/2010/05/08/what-are-id3-tags-all-about/#id3v1
+	internal class Id3V1Tag : ITag {
+
+		private readonly IResourceManager resourceManager;
+
+		public Id3V1Tag( IResourceManager resourceManager ) {
+			this.resourceManager = resourceManager;
+		}
+
+		public string Type => "Id3v1";
+
+		public string Title { get; private set; }
+
+		public string Artist { get; private set; }
+
+		public string Album { get; private set; }
+
+		public string Year { get; private set; }
+
+		public string Comment { get; private set; }
+		
+		public int Track {  get; private set; }
+		
+		public int Genre {  get; private set; }
+
+		public bool ReadTag( string mp3File ) {
+			var tagFound = false;
+			
+			using var fs = File.Open( mp3File, FileMode.Open, FileAccess.Read, FileShare.Read );
+			fs.Seek( -128, SeekOrigin.End );
+
+			using var br = new BinaryReader( fs );
+
+			var buffer = new byte[128];
+			br.Read( buffer, 0, buffer.Length );
+
+			var markerReader = new StringReader( buffer, 0, 2, Encoding.Latin1 );
+			var marker = markerReader.ReadString();
+
+			if ( marker == "TAG" ) {
+				tagFound = true;
+
+				var titleReader = new StringReader( buffer, 3, 32, Encoding.Latin1 );
+				Title = titleReader.ReadString();
+
+				var artistReader = new StringReader( buffer, 33, 62, Encoding.Latin1 );
+				Artist = artistReader.ReadString();
+
+				var albumReader = new StringReader( buffer, 63, 92, Encoding.Latin1 );
+				Album = albumReader.ReadString();
+
+				var yearReader = new StringReader( buffer, 93, 96, Encoding.Latin1 );
+				Year = yearReader.ReadString();
+
+				var commentReader = new StringReader( buffer, 97, 124, Encoding.Latin1 );
+				Comment = commentReader.ReadString();
+
+				Track = buffer[126];
+
+				Genre = buffer[127];
+			}
+
+			return tagFound;
+		}
+	}
+}
--- Mp3TagInfo.cs
+++ Mp3TagInfo.cs
@@ -8,16 +8,22 @@
 			this.resourceManager = resourceManager;
 			this.sortId3V2Frames = sortId3V2Frames;
 
-			Tags = new List<ITag>();
+			Tags = [];
 		}
 
 		public string Mp3File { get; }
 
-		public List<ITag> Tags { get; private set; }
+		public List<ITag> Tags { get; }
 		
 		public void LoadTags() {
 			// Id3v2
 			Tags.Add( new Id3V2Tag( Mp3File, resourceManager, sortId3V2Frames ) );
+
+			// Id3v1
+			var id3V1Tag = new Id3V1Tag( resourceManager );
+			if ( id3V1Tag.ReadTag( Mp3File ) ) {
+				Tags.Add( id3V1Tag );
+			}
 		}
 	}
 }
